a3e7d73c6ec10b6cd4ed412bbb4d162c53406eaa
rohitbhosale20 <justrohit02@gmail.com>
2025-02-16 16:31:45 +0100

    hero-section-updated

--- my-anrufio-project/src/app/home/home.component.ts
+++ my-anrufio-project/src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  AfterViewInit,
+} from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
@@ -11,4 +15,13 @@ import { HeaderComponent } from '../header/header.component';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    const video = document.querySelector('video') as HTMLVideoElement;
+    if (video) {
+      video.muted = true;
+      video.play().catch((error) => console.error('Autoplay failed:', error));
+    }
+  }
+}
